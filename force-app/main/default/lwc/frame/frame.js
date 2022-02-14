5fc62d306d50c9abcdca907f13b68f980576c072
shindegirish <shinde.girish.r@gmail.com>
2022-02-14 22:26:11 +0530

    populated frames

--- force-app/main/default/lwc/frame/frame.js
+++ force-app/main/default/lwc/frame/frame.js
@@ -6,4 +6,20 @@ export default class Frame extends LightningElement {
   @api rightScore;
   @api tenthFrameScore;
   @api totalScore;
+
+  get tenthFrameBox() {
+    return this.frameNumber === 10;
+  }
+
+  get leftScoreClass() {
+    return this.frameNumber === 10
+      ? `slds-col slds-align_absolute-center slds-box slds-size_1-of-3`
+      : `slds-col slds-align_absolute-center slds-size_1-of-2`;
+  }
+
+  get rightScoreClass() {
+    return this.frameNumber === 10
+      ? `slds-col slds-align_absolute-center slds-box slds-size_1-of-3`
+      : `slds-col slds-align_absolute-center slds-box slds-size_1-of-2`;
+  }
 }
--- force-app/main/default/lwc/game/game.js
+++ force-app/main/default/lwc/game/game.js
@@ -5,17 +5,24 @@ export default class Game extends LightningElement {
 
   //reactive properties
 
+  //create 11 pin from 0 to 10
   get pins() {
     return [...Array(11)].map((_, i) => i);
   }
 
   //reactive properties
   // to store all frames
-  @track frames = [];
+  @track frames = [...Array(10)].map((_, idx) => {
+    return {
+      frameNumber: idx + 1
+    };
+  });
 
   //to store rolls
   currentRoll = 0;
   rolls = [];
+
+  //cumulative score of the game
   totalScore;
 
   isSpare(frameIdx) {
@@ -26,24 +33,39 @@ export default class Game extends LightningElement {
     return this.rolls[frameIdx] === 10;
   }
 
+  firstRoll(frameIdx) {
+    return this.rolls[frameIdx];
+  }
+
+  secondRoll(frameIdx) {
+    return this.rolls[frameIdx + 1];
+  }
+
+  nextRoll(frameIdx) {
+    return this.rolls[frameIdx + 2];
+  }
+
   score() {
     let score = 0;
     let frameIdx = 0;
-    [...Array(10)].forEach((f) => {
+    this.frames = [];
+    [...Array(10)].forEach((f, idx) => {
+      const left = this.firstRoll(frameIdx);
+      const right = this.secondRoll(frameIdx);
+      const next = this.nextRoll(frameIdx);
       if (this.isStrike(frameIdx)) {
-        score +=
-          10 +
-          (this.rolls[frameIdx + 1] ?? 0) +
-          (this.rolls[frameIdx + 2] ?? 0);
+        score += 10 + right + next;
+        this.updateFrames(idx, "", "X", score, next);
         frameIdx++;
       } else if (this.isSpare(frameIdx)) {
-        score += 10 + (this.rolls[frameIdx + 2] ?? 0);
+        score += 10 + next;
+        this.updateFrames(idx, left, "/", score, next);
         frameIdx += 2;
       } else {
-        score += (this.rolls[frameIdx] ?? 0) + (this.rolls[frameIdx + 1] ?? 0);
+        score += left + right;
+        this.updateFrames(idx, left, right, score, next);
         frameIdx += 2;
       }
-      console.log(`score after frame no ${frameIdx} is ${score}`);
     });
     return score;
   }
@@ -54,4 +76,42 @@ export default class Game extends LightningElement {
     console.log(JSON.stringify(this.rolls));
     this.totalScore = this.score();
   }
+
+  updateFrames(idx, leftScore, rightScore, totalScore, nextScore) {
+    if (idx < 9) {
+      this.frames.push({
+        frameNumber: idx + 1,
+        leftScore,
+        rightScore,
+        // if score is NaN then pass cumulative as undefined so that score won't be visible in frame
+        totalScore: totalScore || undefined
+      });
+    } else {
+      console.log(
+        JSON.parse(
+          JSON.stringify({ idx, leftScore, rightScore, totalScore, nextScore })
+        )
+      );
+      this.frames.push({
+        frameNumber: idx + 1,
+        leftScore: leftScore === "" ? "X" : leftScore,
+        rightScore,
+        // if score is NaN then pass cumulative as undefined so that score won't be visible in frame
+        totalScore: totalScore || undefined,
+        tenthFrameScore:
+          nextScore === 10
+            ? "X"
+            : leftScore === 10 || leftScore + rightScore === 10
+            ? nextScore
+            : ""
+      });
+    }
+  }
 }
+
+// Step 1 - build the main logic to calculate the score
+// step 2 - build the UI to show details
+// step 3 - integrate the score with UI
+// step   - handle 10th frame 3rd roll
+// step 4 - store the game score in salesforce object
+// step 5 - load the game score from salesforce object
